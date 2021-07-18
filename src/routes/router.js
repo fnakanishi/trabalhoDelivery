9a472ead280f94f9a7fb6187cd6858307081d3a6
fnakanishi <traca.tm@gmail.com>
2021-07-17 21:46:08 -0300

    Commit inicial

---
+++ src/routes/router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const associadoRouter = require('./associadoRouter');
+const mototboyRouter = require('./mototboyRouter');
+const clienteRouter = require('./clienteRouter');
+const entregaRouter = require('./entregaRouter');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	res.send(`It's working`);
+});
+
+router.use('/associados', associadoRouter);
+router.use('/motoboys', mototboyRouter);
+router.use('/clientes', clienteRouter);
+router.use('/entregas', entregaRouter);
+
+module.exports = router;
