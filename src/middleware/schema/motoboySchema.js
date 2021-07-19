6d9f0a434057148b92840e1f95fa44cf66cb7db4
Carlos <43823793+fnakanishi@users.noreply.github.com>
2021-07-19 18:47:16 -0300

    Update motoboySchema.js

--- src/middleware/schema/motoboySchema.js
+++ src/middleware/schema/motoboySchema.js
@@ -4,7 +4,7 @@ const schema = Joi.object().keys({
   id: Joi.number().integer(),
   nome: Joi.string().required(),
   cpf: Joi.string().pattern(/[0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}/).required(),
-  senha: Joi.string().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).min(8).required(),
+  senha: Joi.string().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%*#?&]).{8,}$/).min(8).required(),
   telefone: Joi.string().max(150).required()
 });
 
