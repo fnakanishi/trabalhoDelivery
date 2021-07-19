0049cfde09dfa85b03135d35519a1f5ee1d5dcd8
Rafael Karam <rafael.karam10@gmail.com>
2021-07-19 18:51:35 -0300

    Padrões-seed

--- src/database/seeders/20210719133004-motoboys.js
+++ src/database/seeders/20210719133004-motoboys.js
@@ -7,28 +7,28 @@ module.exports = {
 			[
 				{
 					nome: "José Oliveira",
-					cpf: "382.584.990-25",
+					cpf: "38258499025",
 					senha:"$2a$12$aff3q7/eHpx83jZWJu801eomZBIfCGh1T2GkwuprXAwSCmz84AvZW",
 					telefone: "(41)99896-8745"
 					
 				},
 				{
 					nome: "Marcos Ribeiro",
-					cpf: "004.483.540-04",
+					cpf: "00448354004",
 					senha:"$2a$12$aff3q7/eHpx83jZWJu801eomZBIfCGh1T2GkwuprXAwSCmz84AvZW",
 					telefone: "(41)99745-6321"
 					
 				},
 				{
 					nome: "Carlos Santanna",
-					cpf: "188.381.490-11",
+					cpf: "18838149011",
 					senha:"$2a$12$aff3q7/eHpx83jZWJu801eomZBIfCGh1T2GkwuprXAwSCmz84AvZW",
 					telefone: "(41)99741-2589"
 					
 				},
 				{
 					nome: "Michel Santos",
-					cpf: "608.414.580-90",
+					cpf: "60841458090",
 					senha:"$2a$12$aff3q7/eHpx83jZWJu801eomZBIfCGh1T2GkwuprXAwSCmz84AvZW",
 					telefone: "(41)99787-6251"
 					
--- src/database/seeders/20210719133021-clientes.js
+++ src/database/seeders/20210719133021-clientes.js
@@ -7,23 +7,23 @@ module.exports = {
 			[
 				{
 					nomeEmpresa: "Empresa Cliente1",
-					cnpj: "08.353.824/0001-57",
+					cnpj: "08353824000157",
 					endereco: "Rua João Lech Júnior, 456"
 					
 				},
 				{
 					nomeEmpresa: "Empresa Cliente2",
-					cnpj: "07.702.247/0001-07",
+					cnpj: "07702247000107",
 					endereco: "Rua Radomil Celinski, 74"
 				},
 				{
 					nomeEmpresa: "Empresa Cliente3",
-					cnpj: "98.843.649/0001-79",
+					cnpj: "98843649000179",
 					endereco: "Rua José Bassa, 123"
 				},
 				{
 					nomeEmpresa: "Empresa Cliente4",
-					cnpj: "63.131.719/0001-81",
+					cnpj: "63131719000181",
 					endereco: "Rua Dante Bertoni, 74"
 				},
 			],
